b2e82004884052fb226b8c129d2cb22a70599cbc
Rigoberto Perez <rigotra1984@gmail.com>
2021-03-30 18:36:13 -0400

    corrigiendo ejemplo

--- example/index.tsx
+++ example/index.tsx
@@ -1,8 +1,5 @@
 import { AppRegistry } from 'react-native';
-import { RNTwilioPhone } from 'react-native-twilio-phone';
 import { name as appName } from './app.json';
 import { App } from './src/App';
-
-RNTwilioPhone.handleBackgroundState();
 
 AppRegistry.registerComponent(appName, () => App);
